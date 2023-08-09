4c5dbe18e8cdb536aaf8e5d2ad0d89675f8fccd7
aravindmurugesan2692 <aravindmurugesan2692@gmail.com>
2023-08-09 16:47:55 +0530

    initial comit

---
+++ assignment/src/test/java/springboot/star/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.star.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
